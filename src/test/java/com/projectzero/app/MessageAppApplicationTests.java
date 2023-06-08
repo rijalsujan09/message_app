9034e3c85394f54f7176470aca6a9c5c6048c8f4
Sujan Rijal <rijalsujan@gmail.com>
2023-06-07 21:48:34 -0400

    project setup

---
+++ src/test/java/com/projectzero/app/MessageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectzero.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
