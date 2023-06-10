3402ced6ae9a2b9188769bf8e6b6a8445292dc00
Sujan Rijal <rijalsujan@gmail.com>
2023-06-10 13:56:29 -0400

    properties added

--- src/main/java/com/projectzero/app/MessageAppApplication.java
+++ src/main/java/com/projectzero/app/MessageAppApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessageAppApplication {
+public class MessageAppApplication implements Runnable  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessageAppApplication.class, args);
+		
+		
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Programme Strated");
+		
 	}
 
 }
